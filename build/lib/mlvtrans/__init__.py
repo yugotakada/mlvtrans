ecd2fa36b1145e7446f2a9f49f68719b6eea05a6
yugotakada <yugo.takada1@gmail.com>
2025-02-22 05:15:31 +0900

    change: setup.py

---
+++ build/lib/mlvtrans/__init__.py
@@ -0,0 +1,9 @@
+from .main import (
+    print_desired_logical_S, alg1, comp_basis, phys_S_for_logi_S,  check_compatible,check_S, examples, run_matrix, run_index, run_matrix_auto_coset, run_index_auto_coset, lemma2, main
+)
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "print_desired_logical_S", "alg1", "comp_basis", "phys_S_for_logi_S", "check", "examples", "run_matrix", "run_index", "run_matrix_auto_coset", "run_index_auto_coset", "lemma2", "main"
+]
